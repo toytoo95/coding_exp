02c1b6d41e9015765efb237b46f80f4be2a147f4
toytoo95 <43206942+toytoo95@users.noreply.github.com>
2019-09-24 15:27:10 +0900

    Create Solution1229_암호문2.java

---
+++ swexpert/Solution1229_암호문2.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+ 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        for (int test_case = 1; test_case < 11; test_case++) {
+            int n = sc.nextInt();
+            List<Integer> code = new ArrayList();
+            List<Integer> arr = new ArrayList<>();
+            
+            for (int i = 0; i < n; i++) {
+                code.add(sc.nextInt());
+            }
+            
+            int a = sc.nextInt();
+            for (int i = 0; i < a; i++) {
+                char type = sc.next().charAt(0);
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                 
+                if(type == 'D') {
+                    for (int j = 0; j < y; j++) {
+                        code.remove(x);
+                    }
+                }
+ 
+                else {
+                    for (int j = 0; j < y; j++) {
+                        arr.add(sc.nextInt());  
+                    }
+                    if(type == 'I') {
+                        while(arr.size() != 0) {
+                            code.add(x, arr.remove(arr.size()-1));
+                        }
+                    }
+                }
+            }
+            
+            System.out.print("#"+test_case+" ");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(code.remove(0)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
